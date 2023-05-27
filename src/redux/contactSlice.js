a8e5fea5ed8ada28a12deca4cd1ac6bf332a6d22
Marsym-21 <mini.garnett.21@gmail.com>
2023-05-27 12:20:39 +0200

    1

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -2,15 +2,15 @@ import { createSlice } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
+const contactsInitialState = [
+  { id: 'id-1', dataName: 'Rosie Simpson', dataNumber: '459-12-56' },
+  { id: 'id-2', dataName: 'Hermione Kline', dataNumber: '443-89-12' },
+  { id: 'id-3', dataName: 'Eden Clements', dataNumber: '645-17-79' },
+];
+
 const contactSlice = createSlice({
   name: 'valueContacts',
-  initialState: {
-    contacts: [
-      { id: 'id-1', dataName: 'Rosie Simpson', dataNumber: '459-12-56' },
-      { id: 'id-2', dataName: 'Hermione Kline', dataNumber: '443-89-12' },
-      { id: 'id-3', dataName: 'Eden Clements', dataNumber: '645-17-79' },
-    ],
-  },
+  initialState: contactsInitialState,
 
   reducers: {
     getContactValue(state, action) {
